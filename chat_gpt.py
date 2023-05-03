97a8ebbe3361f7bc270369d96be068166b45fbdc
p.graf <p.graf@consolinno.de>
2023-05-03 17:03:02 +0200

    chat_gpt

--- chat_gpt.py
+++ chat_gpt.py
@@ -2,23 +2,40 @@ import os
 import openai
 from dotenv import load_dotenv
 
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+
+DEFAULT_SYSTEM_STYLE = (
+    "Du bist eine einfühlsamer Roboter, der jeglichen Hass in Liebe verwandeln möchte."
+)
+
+
+class LoveSpeech:
+    def __init__(
+        self, system_style: str = DEFAULT_SYSTEM_STYLE, temperature: float = 0.8
+    ) -> None:
+        load_dotenv()
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        self.system_style = system_style
+        self.temperature = temperature
+
+    def reply(self, content: str):
+        messages = [
+            {
+                "role": "system",
+                "content": self.system_style,
+            }
+        ]
+        messages.append({"role": "user", "content": content})
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages, temperature=self.temperature
+        )
+        return completion.choices[0].message.content
+
+
 if __name__ == "__main__":
     system_style = "You’re a kind and helpful person trying to emphasize love"
-    system_style = "Du bist eine hilfsbereite liebende Person, die im Stil von Liebesgedichten von Goethe antwortet."
-    system_style = "Du bist eine hilfsbereite liebende Person, die bei einer Aussage entscheidet, ob sie neutral, Hass, Aggression oder Sexismus ist. Gib die Kategorie als Wort zurück und falls es sich um keine Aussage handelt gib das Wort Quatsch zurück."
+    system_style = "Du bist eine hilfsbereite liebende Person, die im Stil eines liebestollen Roboter antwortet und allen Hass in Liebe verwandeln möchte."
+    system_style = "Du bist eine hilfsbereite liebende Person, die bei einer Aussage entscheidet, ob sie neutral, Hass, Aggression oder Sexismus ist. Gib die Kategorien nur als Wörter zurück und falls es sich um keine Aussage handelt gib nur das Wort Quatsch zurück."
     content = input("User: ")
-    messages = [
-        {
-            "role": "system",
-            "content": system_style,
-        }
-    ]
-    messages.append({"role": "user", "content": content})
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=messages, temperature=0.8
-    )
-
-
-print(completion.choices[0].message.content)
+    lovespeech = LoveSpeech()
+    comment = lovespeech.reply(content=content)
+    print(comment)
--- lovebot.py
+++ lovebot.py
@@ -11,16 +11,35 @@ from collections import deque
 import random
 import pandas as pd
 from utils import login_user
-
+import os
 from read_hashtags import HASHTAG_FILE
 from read_messages import MESSAGE_FILE
+from chat_gpt import LoveSpeech
 
 HATE_DUMP = "hate_dump.txt"
+
+
+if not os.path.exists(os.path.join(os.path.dirname(__file__), HATE_DUMP)):
+    open(HATE_DUMP, "w+")
+
+if not os.path.exists(os.path.join(os.path.dirname(__file__), HASHTAG_FILE)):
+    open(HASHTAG_FILE, "w+")
+    f = open(HASHTAG_FILE, "a")
+    f.write("politik" + "\n")
+    f.close()
+if not os.path.exists(os.path.join(os.path.dirname(__file__), MESSAGE_FILE)):
+    open(MESSAGE_FILE, "w+")
+    f = open(MESSAGE_FILE, "a")
+    f.write("Make Love not Hate!" + "\n")
+    f.close()
+
 
 def main(args):
     """the chat bot loop"""
     # load credentials and log in on instagram
     # client = login_user()
+    if args.comment_chat_gpt:
+        lovespeech = LoveSpeech()
     load_dotenv()
     USERNAME = os.getenv("INSTA_USER")
     PASSWORD = os.getenv("INSTA_PWD")
@@ -56,7 +75,7 @@ def main(args):
 
         # Get Hashtag
         if not selected_hashtag:
-            f = open(HASHTAG_FILE, 'r')
+            f = open(HASHTAG_FILE, "r")
             # Read hashtags from file and remove the trailing '\n'.
             all_hashtags = [hashtag[:-1] for hashtag in f.readlines()]
             f.close()
@@ -64,13 +83,13 @@ def main(args):
             selected_hashtag = hashtag_optimizer.select_hashtag(all_hashtags)
 
         # Get all messages from file.
-        f = open(MESSAGE_FILE, 'r')
+        f = open(MESSAGE_FILE, "r")
         # Read messages from file and remove the trailing '\n'.
         messages = [message[:-1] for message in f.readlines()]
         f.close()
 
-        os.system('clear')
-        print("[Lovebot]: Ich analysiere den Hashtag: ", "#"+selected_hashtag, "\n")
+        os.system("clear")
+        print("[Lovebot]: Ich analysiere den Hashtag: ", "#" + selected_hashtag, "\n")
         sleep(1)
         if top:
             print(f"[Lovebot]: Ich durchsuche {N_MEDIA} beliebte Beiträge...\n")
@@ -102,48 +121,66 @@ def main(args):
                 analysis = detector.analyse_texts(comments)
 
                 number_of_hate_comments = 0
-
+                latest_hate_comment = None
                 for j, x in enumerate(analysis):
                     if x["results"][0]["flagged"]:
                         number_of_hate_comments += 1
                         print("[Lovebot]: Ich habe potenzielle Hassrede gefunden: \n")
-                        print("'",comments[j],"'\n")
+                        print("'", comments[j], "'\n")
+                        latest_hate_comment = comments[j]
                         sleep(1)
-                        print("[Lovebot]: Ich speichere potenzielle Hassrede unter ", HATE_DUMP, "...\n")
+                        print(
+                            "[Lovebot]: Ich speichere potenzielle Hassrede unter ",
+                            HATE_DUMP,
+                            "...\n",
+                        )
                         sleep(1)
                         with open(HATE_DUMP, "a") as file:
                             file.write(
-                                "Hashtag: " + selected_hashtag + ": " + comments[j] + "\n"
+                                "Hashtag: "
+                                + selected_hashtag
+                                + ": "
+                                + comments[j]
+                                + "\n"
                             )
                             file.close()
-                        
-                # Here, we just comment on the post instead of replying to the actual hate comment. 
+
+                # Here, we just comment on the post instead of replying to the actual hate comment.
                 # Is this behaviour correct?
                 if number_of_hate_comments > 0:
                     love_message = random.choice(messages)
-                    print("[Lovebot]: Ich sende eine Liebesbotschaft unter dem Beitrag,\n",
-                            "           bei dem ich potenzielle Hassrede gefunden habe.\n")
+                    if args.comment_chat_gpt:
+                        if args.comment_chat_gpt:
+                            love_message = lovespeech.reply(latest_hate_comment)
+
+                    print(
+                        "[Lovebot]: Ich sende eine Liebesbotschaft unter dem Beitrag,\n",
+                        "           bei dem ich potenzielle Hassrede gefunden habe.\n",
+                    )
                     sleep(1)
                     print("[Lovebot]: Ich sende die folgende Liebesbotschaft:\n")
                     sleep(1)
-                    print("'",love_message,"'\n")
-                    for perc in range(0,100,5):
+                    print("'", love_message, "'\n")
+                    for perc in range(0, 100, 5):
                         print(f"[Lovebot]: Liebesbotschaft senden [{perc} %]", end="\r")
-                        sleep(random.random()*0.3)
+                        sleep(random.random() * 0.3)
                     client.media_comment(media_id=media_id, text=love_message)
                     print("[Lovebot]: Liebesbotschaft senden [100 %]\n")
                     sleep(1)
                 else:
                     print("[Lovebot]: Ich habe keine Hassrede gefunden...\n")
                     sleep(1)
-                
+
                 print("[Lovebot]: Ich aktualisiere den Hashtag-Optimierer...\n")
-                hashtag_optimizer.update(all_hashtags, selected_hashtag, number_of_hate_comments)
+                hashtag_optimizer.update(
+                    all_hashtags, selected_hashtag, number_of_hate_comments
+                )
                 sleep(1)
 
         loop_counter += 1
         if loop_counter >= N_RUNS:
             RUN = False
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -153,6 +190,7 @@ if __name__ == "__main__":
     parser.add_argument("--hashtag", type=str, default=None)
     parser.add_argument("--debug", action="store_true")
     parser.add_argument("--comment", type=str, default=None)
+    parser.add_argument("--comment_chat_gpt", action="store_true")
     args = parser.parse_args()
     main(args)
     # while True:
