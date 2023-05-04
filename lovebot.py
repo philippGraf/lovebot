d8bc05c94fd000ff3265ea529c0f5164f462a7a9
Samuel Ardaya-Lieb <s.ardayalieb@consolinno.de>
2023-05-04 12:37:22 +0200

    set client as parameter for runtime

--- lovebot.py
+++ lovebot.py
@@ -33,36 +33,28 @@ if not os.path.exists(os.path.join(os.path.dirname(__file__), MESSAGE_FILE)):
     f.write("Make Love not Hate!" + "\n")
     f.close()
 
-
-def main(args):
+def main(client, args):
     """the chat bot loop"""
     # load credentials and log in on instagram
-    # client = login_user()
     if args.comment_chat_gpt:
         lovespeech = LoveSpeech()
-    load_dotenv()
-    USERNAME = os.getenv("INSTA_USER")
-    PASSWORD = os.getenv("INSTA_PWD")
-    client = Client()
-    # adds a random delay between 2 and 4 seconds after each request
-    client.delay_range = [2,4]
-    client.login(USERNAME, PASSWORD)
     # -----------------------------------------------------
     # runtime arguments
+    RUN = True
     N_MEDIA = args.n_media
     if args.n_runs is None:
         N_RUNS = Inf
     else:
         N_RUNS = abs(args.n_runs)
-    RUN = True
+    
     loop_counter = 0
-    comment = "make love" if not args.comment else args.comment
+
     # ------------------------------------------------------
     # load the hatespeach detector
     detector = OpenAiModerator(debug=args.debug)
     # ------------------------------------------------------
     # load the hashtag optimizer
-    hashtag_optimizer = HashtagOptimizer()
+    hashtag_optimizer = HashtagOptimizer(alpha=5)
     # ------------------------------------------------------
 
     selected_hashtag = args.hashtag
@@ -73,8 +65,8 @@ def main(args):
     top = True
     while RUN:
         # We read hashtags and messages inside the main loop
-        # because other processes are adding both continuously.
-
+        # while other processes are adding both continuously.
+        top = not top
         # Get Hashtag
         if not selected_hashtag:
             f = open(HASHTAG_FILE, "r")
@@ -101,7 +93,8 @@ def main(args):
             medias = client.hashtag_medias_recent(
                 selected_hashtag, amount=N_MEDIA
             )  # here it would be also possible to sample from related hashtags and to scan them also
-        top = not top
+        number_of_hate_comments = 0
+
         for media in medias:
             ANALYSE_MEDIA = True
             media_id = client.media_id(media.pk)
@@ -111,6 +104,8 @@ def main(args):
             except ValueError as e:
                 media_buffer.append(media.pk)
             if ANALYSE_MEDIA:
+                print("[Lovebot]: Ich sehe mir die Kommentare unter einem Beitrag an...\n")
+                sleep(1)
                 comm_obs = client.media_comments(media_id=media_id, amount=100)
                 sleep(args.sleep)
                 comments = list(
@@ -121,22 +116,16 @@ def main(args):
 
                 # Analyse the received comments...
                 analysis = detector.analyse_texts(comments)
-
-                number_of_hate_comments = 0
+                hate_under_current_post = False
                 latest_hate_comment = None
                 for j, x in enumerate(analysis):
                     if x["results"][0]["flagged"]:
                         number_of_hate_comments += 1
+                        hate_under_current_post = True
                         print("[Lovebot]: Ich habe potenzielle Hassrede gefunden: \n")
                         print("'", comments[j], "'\n")
                         latest_hate_comment = comments[j]
-                        sleep(1)
-                        print(
-                            "[Lovebot]: Ich speichere potenzielle Hassrede unter ",
-                            HATE_DUMP,
-                            "...\n",
-                        )
-                        sleep(1)
+                        sleep(5)
                         with open(HATE_DUMP, "a") as file:
                             file.write(
                                 "Hashtag: "
@@ -147,37 +136,33 @@ def main(args):
                             )
                             file.close()
 
-                # Here, we just comment on the post instead of replying to the actual hate comment.
-                # Is this behaviour correct?
-                if number_of_hate_comments > 0:
+                if hate_under_current_post:
                     love_message = random.choice(messages)
                     if args.comment_chat_gpt:
                         if args.comment_chat_gpt:
                             love_message = lovespeech.reply(latest_hate_comment)
 
-                    print(
-                        "[Lovebot]: Ich sende eine Liebesbotschaft unter dem Beitrag,\n",
-                        "           bei dem ich potenzielle Hassrede gefunden habe.\n",
-                    )
-                    sleep(1)
-                    print("[Lovebot]: Ich sende die folgende Liebesbotschaft:\n")
+                    print("[Lovebot]: Ich sende folgende Liebesbotschaft unter dem Beitrag:\n")
                     sleep(1)
                     print("'", love_message, "'\n")
                     for perc in range(0, 100, 5):
                         print(f"[Lovebot]: Liebesbotschaft senden [{perc} %]", end="\r")
                         sleep(random.random() * 0.3)
-                    client.media_comment(media_id=media_id, text=love_message)
-                    print("[Lovebot]: Liebesbotschaft senden [100 %]\n")
+                    try:
+                        client.media_comment(media_id=media_id, text=love_message)
+                        print("[Lovebot]: Liebesbotschaft senden [100 %]\n")
+                    except:
+                        print("[Lovebot]: Liebesbotschaft senden fehlgeschlagen. Das hat leider nicht geklappt :(\n")
                     sleep(1)
                 else:
                     print("[Lovebot]: Ich habe keine Hassrede gefunden...\n")
                     sleep(1)
 
-                print("[Lovebot]: Ich aktualisiere den Hashtag-Optimierer...\n")
-                hashtag_optimizer.update(
-                    all_hashtags, selected_hashtag, number_of_hate_comments
-                )
-                sleep(1)
+            print("[Lovebot]: Ich aktualisiere den Hashtag-Optimierer...\n")
+            hashtag_optimizer.update(
+                all_hashtags, selected_hashtag, (number_of_hate_comments/N_MEDIA)
+            )
+            sleep(1)
 
         loop_counter += 1
         if loop_counter >= N_RUNS:
@@ -191,13 +176,18 @@ if __name__ == "__main__":
     parser.add_argument("--sleep", type=int, default=5)
     parser.add_argument("--hashtag", type=str, default=None)
     parser.add_argument("--debug", action="store_true")
-    parser.add_argument("--comment", type=str, default=None)
     parser.add_argument("--comment_chat_gpt", action="store_true")
     args = parser.parse_args()
-    main(args)
-    # while True:
-    #     try:
-    #         main(args)
-    #     except Exception as e:
-    #         print(e)
-    #         sleep(30)
+
+    load_dotenv()
+    USERNAME = os.getenv("INSTA_USER")
+    PASSWORD = os.getenv("INSTA_PWD")
+    client = Client()
+    # adds a random delay between 2 and 4 seconds after each request
+    client.delay_range = [2,4]
+    client.login(USERNAME, PASSWORD)
+    
+    try:
+        main(client, args)
+    except KeyboardInterrupt:
+        client.logout()
